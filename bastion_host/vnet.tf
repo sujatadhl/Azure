e534d99f58076c220a8e15b029b9a8e84ca22cbf
sujatadhl <dahalsujata52@gmail.com>
2024-08-13 09:58:19 +0545

    Changed name in nsg security rule

--- bastion_host/vnet.tf
+++ bastion_host/vnet.tf
@@ -19,7 +19,7 @@ resource "azurerm_network_security_group" "sujata_nsg" {
     location            = azurerm_resource_group.sujata_rg.location
     resource_group_name = azurerm_resource_group.sujata_rg.name
     security_rule {
-        name                       = "SSH"
+        name                       = "RDP"
         priority                   = 1001
         direction                  = "Inbound"
         access                     = "Deny"
